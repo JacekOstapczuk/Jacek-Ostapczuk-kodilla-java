2880ff5be7dc6b783eb2b0fb536f636877db0586
Jacek Ostapczuk <jacekostapczuk86@gmail.com>
2022-09-12 21:37:45 +0200

    6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,36 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
-    }
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        // tu umieść kolejny test ->
+        System.out.println("Test - pierwszy test jednostkowy:");
+
+        Calculator calculations = new Calculator();
+        int addTest =  calculations.addAtoB(3,6);
+         if (addTest == 9) {
+             System.out.println("test dodawania - pozytywny");
+         } else  {System.out.println("test dodawnia - negatywny");
+         }
+
+        int substractTest =calculations.substractAfromB (19, 4);
+        if (substractTest==15) {
+            System.out.println("test odejmowania - pozytywny");
+        }   else { System.out.println("test odejmowania - negatywny");
+        }
+        }
+
 }
